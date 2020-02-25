0b0d449f3e2081e02c2e0db33ff1a09c7846ae77
Yiğit Sadıç <yigit.sadic@globalmiles.com>
2020-02-25 11:45:54 +0300

    Applied mask for first names.

--- lib/name_masker.rb
+++ lib/name_masker.rb
@@ -2,5 +2,26 @@ require "name_masker/version"
 
 module NameMasker
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.mask_first_name(name)
+    return '*' if name.length < 2
+
+    masked_part = case name.length
+                  when 2..3
+                    '*'
+                  when 4
+                    '**'
+                  else
+                    '***'
+                  end
+
+    base_string = case name.length
+                  when 2
+                    name[0]
+                  else
+                    name[0..1]
+                  end
+
+    [base_string, masked_part].join
+  end
 end
--- test/name_masker_test.rb
+++ test/name_masker_test.rb
@@ -5,7 +5,20 @@ class NameMaskerTest < Minitest::Test
     refute_nil ::NameMasker::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_has_mask_first_name_method
+    assert ::NameMasker.respond_to?(:mask_first_name)
+  end
+
+  def test_it_masks_empty_first_name_correctly
+    assert_equal '*', ::NameMasker.mask_first_name('')
+  end
+
+  def test_it_masks_first_name_correctly
+    names = %w[Ali Kaci Natasha Abraham G]
+    expected_results = %w[Al* Ka** Na*** Ab*** *]
+
+    names.each_with_index do |name, index|
+      assert_equal expected_results[index], ::NameMasker.mask_first_name(name)
+    end
   end
 end
